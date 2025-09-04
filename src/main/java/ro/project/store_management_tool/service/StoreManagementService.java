10bb05e864ead939cde6b9750b118dff76aca7a3
Andreea Alina Cornea <andreeaalinacornea@MacBook-Air-Andreea.local>
2025-09-04 20:44:47 +0300

    optimize imports

--- src/main/java/ro/project/store_management_tool/service/StoreManagementService.java
+++ src/main/java/ro/project/store_management_tool/service/StoreManagementService.java
@@ -1,6 +1,5 @@
 package ro.project.store_management_tool.service;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
--- src/test/java/ro/project/store_management_tool/StoreManagementToolApplicationTests.java
+++ src/test/java/ro/project/store_management_tool/StoreManagementToolApplicationTests.java
@@ -2,19 +2,12 @@ package ro.project.store_management_tool;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.web.WebMergedContextConfiguration;
-import ro.project.store_management_tool.config.SecurityConfig;
 
 @SpringBootTest
 @Import(StoreManagementToolApplicationTests.SecurityTestConfig.class)
